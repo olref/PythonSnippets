533ad43b87e52318425b0588990636b3fa2178b0
Olivier Refreges <olivier.refreges@gmail.com>
2012-08-19 14:17:03 +0200

    basic curses with python

---
+++ basicCurses.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import curses
+
+def main(self):
+    screen = curses.initscr()
+    screen.border(0)
+    
+    #main loop
+    while True:
+        #write title on the first line (x=0)
+        screen.addstr(0,15, 'My great menu')
+        screen.addstr(8,2, '1 - What is the answer to life ? ')
+        screen.addstr(9,2, '2 - Give me my computer uptime')
+        screen.addstr(15,2, 'q - Bye Bye')
+        #recupération des saisies utilisateurs
+        event = screen.getch()
+        if event == ord("q"):
+            break
+        elif event == ord("1"):
+            #just print 42
+        elif event == ord("2"):
+            #print uptime  
+    #restore your terminal in its original state
+    curses.endwin()
+
+if __name__ == '__main__':
+    #use curses.wrapper to keep your terminal "safe"
+    curses.wrapper(main)
